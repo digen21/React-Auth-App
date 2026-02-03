fad5f7587cb8ecd63646dafb78010b5129607e56
Digen More <digenmore4@gmail.com>
2026-02-04 02:36:57 +0530

    fix: update environment variable schema to conditionally require values based on NODE_ENV

--- server/src/config/envVariable.ts
+++ server/src/config/envVariable.ts
@@ -10,13 +10,37 @@ const envSchema = Joi.object({
   EXPIRY_TIME: Joi.string().default("10d"),
   MONGO_URI: Joi.string().uri().required(),
   MONGO_URI_TEST: Joi.string().uri().required(),
-  GOOGLE_CLIENT_ID: Joi.string().required(),
-  GOOGLE_CLIENT_SECRET: Joi.string().required(),
-  GOOGLE_CALLBACK_URL: Joi.string().required(),
-  MAIL_HOST: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
+  GOOGLE_CLIENT_SECRET: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
+  GOOGLE_CALLBACK_URL: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
+  MAIL_HOST: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
   MAIL_PORT: Joi.number().default(587),
-  MAIL_USER: Joi.string().required(),
-  MAIL_PASSWORD: Joi.string().required(),
+  MAIL_USER: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
+  MAIL_PASSWORD: Joi.when("NODE_ENV", {
+    is: "test",
+    then: Joi.string().optional(),
+    otherwise: Joi.string().required(),
+  }),
   EMAIL_FROM: Joi.string(),
 }).unknown();
 
@@ -39,11 +63,11 @@ const env = {
   isDev: envVars.NODE_ENV === "development",
   isTest: envVars.NODE_ENV === "test",
   JWT_TOKEN: envVars.JWT_TOKEN,
-  MAIL_HOST: envVars.SMTP_HOST,
-  MAIL_PORT: envVars.SMTP_PORT,
-  MAIL_USER: envVars.SMTP_USER,
-  MAIL_PASSWORD: envVars.SMTP_PASS,
-  EMAIL_FROM: envVars.SMTP_FROM_NAME || "no-reply@travel-buddy",
+  MAIL_HOST: envVars.MAIL_HOST,
+  MAIL_PORT: envVars.MAIL_PORT,
+  MAIL_USER: envVars.MAIL_USER,
+  MAIL_PASSWORD: envVars.MAIL_PASSWORD,
+  EMAIL_FROM: envVars.EMAIL_FROM || "no-reply@travel-buddy",
   EXPIRY_TIME: envVars.EXPIRY_TIME || "10d",
 };
 
